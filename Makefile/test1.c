3c734f6592243227fc4ae03cb61b4ba4f714f70e
weienjun <2519812358@qq.com>
2018-05-13 00:01:03 +0800

    makefile

---
+++ Makefile/test1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"test.h"
+
+int main()
+{
+	int num = 0;
+	num = Add(1,2);
+	printf("%d \n",num);
+
+	return 0;
+}
---
+++ Makefile/test2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"test.h"
+
+int main()
+{
+
+	int num2 = 0;
+	num2 = Add(3,4);
+	printf("%d\n",num2);
+	return 0;
+}
